33e5aea4cec563b8395d0ec677002d2324ff61b8
Siobhan <siobhan.harley@gmail.com>
2017-02-17 15:14:51 -0500

    Done.

--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,2 +1,7 @@
 class AddArtistToSongs < ActiveRecord::Migration
+
+  def change
+    add_column :songs, :artist, :string
+  end
+
 end
---
+++ db/migrate/05_add_genre_to_songs.rb
@@ -0,0 +1,7 @@
+class AddGenreToSongs < ActiveRecord::Migration
+
+  def change
+    add_column :songs, :genre_id, :integer
+  end
+
+end
